03b3c2f2676808d1fef2d470c8c7f3f05330a2b1
mevctrader <mevctrader@gmail.com>
2022-09-16 21:35:11 +0000

    primer commit

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,6 @@ import "../style/index.css";
         github: null,
         linkedin: null,
         instagram: null,
-
         name: null,
         lastname: null,
         role: null,
@@ -33,17 +32,29 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+          <h1> ${variables.name ? variables.name : ""} ${
+    variables.lastname ? variables.lastname : " "
+  }
+          </h1>
+          <h2>${variables.role ? variables.role : ""}  </h2>
+          <h3>${variables.country ? variables.country : ""},
+          ${variables.city ? variables.city : ""} </h3>
+          <ul class=" ${variables.socialMediaPosition}">
+          <li><a href="https://twitter.com/${
+            variables.twitter ? variables.twitter : " "
+          }"><i class="fab fa-twitter"></i></a></li>
+       <li><a href="https://github.com/${
+         variables.github ? variables.github : " "
+       }"><i class="fab fa-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/${
+            variables.linkedin ? variables.linkedin : " "
+          }"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="https://instagram.com/${
+            variables.instagram ? variables.inst : " "
+          }"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+  `;
 }
 
 /**
@@ -56,7 +67,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/men/43.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
